85f52d26d9d697b0df40c6cadfc9faf76ab628d5
Carlos Hernandez <carlosherandezarques@gmail.com>
2020-04-20 22:00:25 +0200

    initial commit

---
+++ site/components/src/main/java/io/cmsexperiments/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package io.cmsexperiments.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="cmsplayground:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
